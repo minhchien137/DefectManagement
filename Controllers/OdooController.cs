b57dac960c4a750e8bc92c6b812e0551eb99d7cd
Minh Chien <bmc13072001@gmail.com>
2025-08-15 10:07:34 +0700

    css navbar

--- Controllers/OdooController.cs
+++ Controllers/OdooController.cs
@@ -13,7 +13,7 @@ public class OdooController : ControllerBase
     private readonly HttpClient _httpClient;
     private const string OdooApiUrl = "https://sigmaworldwide.io/web/dataset/call_kw/mrp.production/web_search_read";
 
-    // private const string OdooApiNameEmployer = "https://sigmaworldwide.io/web/dataset/call_kw/hr.employee/web_search_read";
+    private const string OdooApiNameEmployer = "https://sigmaworldwide.io/web/dataset/call_kw/hr.employee/web_search_read";
 
     public OdooController(HttpClient httpClient)
     {
@@ -21,114 +21,113 @@ public class OdooController : ControllerBase
     }
 
 
-    // // API GET tên nhân viên dựa vào SVN Code
+    // API GET tên nhân viên dựa vào SVN Code
 
-    // [HttpGet("SearchName")]
-    // public async Task<IActionResult> SearchName([FromQuery] string nameEmployer)
-    // {
-    //     string nameJson = $@"
-    //         {{
-    //             ""id"": 13,
-    //             ""jsonrpc"": ""2.0"",
-    //             ""method"": ""call"",
-    //             ""params"": {{
-    //                 ""model"": ""hr.employee"",
-    //                 ""method"": ""web_search_read"",
-    //                 ""args"": [],
-    //                 ""kwargs"": {{
-    //                     ""limit"": 80,
-    //                     ""offset"": 0,
-    //                     ""order"": """",
-    //                     ""context"": {{
-    //                         ""lang"": ""vi_VN"",
-    //                         ""tz"": ""Asia/Ho_Chi_Minh"",
-    //                         ""uid"": 2,
-    //                         ""allowed_company_ids"": [1],
-    //                         ""bin_size"": true,
-    //                         ""chat_icon"": true
-    //                     }},
-    //                     ""count_limit"": 10001,
-    //                     ""domain"": [
-    //                         ""|"",
-    //                         [""name"", ""ilike"", ""{nameEmployer}""],
-    //                     ],
-    //                     ""fields"": [
-    //                         ""__last_update"", ""activity_exception_decoration"", ""activity_exception_icon"",
-    //                         ""activity_state"", ""activity_summary"", ""activity_type_icon"", ""activity_type_id"",
-    //                         ""id"", ""hr_presence_state"", ""seniority_years"", ""user_id"", ""user_partner_id"",
-    //                         ""hr_icon_display"", ""show_hr_icon_display"", ""image_128"", ""current_leave_id"",
-    //                         ""current_leave_state"", ""leave_date_from"", ""leave_date_to"", ""is_absent"",
-    //                         ""image_1024"", ""name"", ""job_title"", ""category_ids"", ""work_email"",
-    //                         ""work_phone"", ""seniority_message"", ""activity_ids""
-    //                     ]
-    //                 }}
-    //             }}
-    //         }}";
+    [HttpGet("SearchName")]
+    public async Task<IActionResult> SearchName([FromQuery] string nameEmployer)
+    {
+        string nameJson = $@"
+            {{
+                ""id"": 13,
+                ""jsonrpc"": ""2.0"",
+                ""method"": ""call"",
+                ""params"": {{
+                    ""model"": ""hr.employee"",
+                    ""method"": ""web_search_read"",
+                    ""args"": [],
+                    ""kwargs"": {{
+                        ""limit"": 80,
+                        ""offset"": 0,
+                        ""order"": """",
+                        ""context"": {{
+                            ""lang"": ""vi_VN"",
+                            ""tz"": ""Asia/Ho_Chi_Minh"",
+                            ""uid"": 2,
+                            ""allowed_company_ids"": [1],
+                            ""bin_size"": true,
+                            ""chat_icon"": true
+                        }},
+                        ""count_limit"": 10001,
+                        ""domain"": [
+                            [""name"", ""ilike"", ""{nameEmployer}""],
+                        ],
+                        ""fields"": [
+                            ""__last_update"", ""activity_exception_decoration"", ""activity_exception_icon"",
+                            ""activity_state"", ""activity_summary"", ""activity_type_icon"", ""activity_type_id"",
+                            ""id"", ""hr_presence_state"", ""seniority_years"", ""user_id"", ""user_partner_id"",
+                            ""hr_icon_display"", ""show_hr_icon_display"", ""image_128"", ""current_leave_id"",
+                            ""current_leave_state"", ""leave_date_from"", ""leave_date_to"", ""is_absent"",
+                            ""image_1024"", ""name"", ""job_title"", ""category_ids"", ""work_email"",
+                            ""work_phone"", ""seniority_message"", ""activity_ids""
+                        ]
+                    }}
+                }}
+            }}";
 
-    //     var jsonContentName = new StringContent(nameJson, Encoding.UTF8, "application/json");
+        var jsonContentName = new StringContent(nameJson, Encoding.UTF8, "application/json");
 
-    //     try
-    //     {
-    //         // Tạo request riêng để thêm cookie cho request này thôi
-    //         using var request = new HttpRequestMessage(HttpMethod.Post, OdooApiNameEmployer)
-    //         {
-    //             Content = jsonContentName
-    //         };
+        try
+        {
+            // Tạo request riêng để thêm cookie cho request này thôi
+            using var request = new HttpRequestMessage(HttpMethod.Post, OdooApiNameEmployer)
+            {
+                Content = jsonContentName
+            };
 
-    //         // Thêm cookie cho request này
-    //         var cookie = "frontend_lang=vi_VN; session_id=cc48f91eadf3641a29d02e5b5ebd7e623ceeac11; cids=1";
-    //         request.Headers.Add("Cookie", cookie);
+            // Thêm cookie cho request này
+            var cookie = "frontend_lang=vi_VN; session_id=cc48f91eadf3641a29d02e5b5ebd7e623ceeac11; cids=1";
+            request.Headers.Add("Cookie", cookie);
 
-    //         var response = await _httpClient.SendAsync(request);
-    //         response.EnsureSuccessStatusCode();
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
 
-    //         var responseBody = await response.Content.ReadAsStringAsync();
+            var responseBody = await response.Content.ReadAsStringAsync();
 
-    //         // Trích xuất product code từ response
-    //         var name = ExtractNameemployer(responseBody);
+            // Trích xuất product code từ response
+            var name = ExtractNameemployer(responseBody);
 
-    //         if (!string.IsNullOrEmpty(name))
-    //         {
-    //             return Ok(new { name = name });
-    //         }
-    //         else
-    //         {
-    //             return NotFound(new { message = "Product code not found" });
-    //         }
-    //     }
-    //     catch (HttpRequestException ex)
-    //     {
-    //         return StatusCode(500, new { message = "Error calling Odoo API", details = ex.Message });
-    //     }
+            if (!string.IsNullOrEmpty(name))
+            {
+                return Ok(new { name = name });
+            }
+            else
+            {
+                return NotFound(new { message = "Name Employer not found" });
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            return StatusCode(500, new { message = "Error calling Odoo API", details = ex.Message });
+        }
 
-    // }
+    }
 
-    // private string ExtractNameemployer(string jsonResponse)
-    // {
-    //     try
-    //     {
-    //         var jsonObj = JObject.Parse(jsonResponse);
-    //         var records = jsonObj["result"]?["records"] as JArray;
+    private string ExtractNameemployer(string jsonResponse)
+    {
+        try
+        {
+            var jsonObj = JObject.Parse(jsonResponse);
+            var records = jsonObj["result"]?["records"] as JArray;
 
-    //         if (records != null && records.Count > 0)
-    //         {
-    //             var firstRecord = records[0];
-    //             var nameValue = firstRecord["name"]?.ToString();
+            if (records != null && records.Count > 0)
+            {
+                var firstRecord = records[0];
+                var nameValue = firstRecord["name"]?.ToString();
 
-    //             if (!string.IsNullOrEmpty(nameValue))
-    //             {
-    //                 return nameValue;
-    //             }
-    //         }
+                if (!string.IsNullOrEmpty(nameValue))
+                {
+                    return nameValue;
+                }
+            }
 
-    //         return null;
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"Error extracting name employer: {ex.Message}");
-    //         return null;
-    //     }
-    // }
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error extracting name employer: {ex.Message}");
+            return null;
+        }
+    }
 
     [HttpGet("search")]
     public async Task<IActionResult> Search([FromQuery] string productionCode)
--- obj/Debug/net8.0/DefectManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/DefectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad402e521940a2b52d8ac9b162f559e7d0eb2da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ccb2181d8966b468305f355abc02fc9e2adc37")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
